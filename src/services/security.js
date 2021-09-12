f0dbfe195b22e3f304378673b09e0b97020fd229
Alan Coimbra <algcoimbra01@gmail.com>
2021-09-11 23:25:38 -0300

    Adicionando Imagens e Arquivo de Seguranca

---
+++ src/services/security.js
@@ -0,0 +1,15 @@
+import { api } from "./api";
+
+const USER_KEY = "@user";
+
+export const signIn = (user) => {
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+
+    api.defau;ts.headers.common("authorization") = `Bearer ${user.token}`;
+}
+
+export const getUser = () => {
+    const { student } = JSON.parse(localStorage.getItem(USER_KEY));
+
+    return student;
+}
